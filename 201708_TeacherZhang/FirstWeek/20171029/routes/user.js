e2ca189075e438bdd2e33d5fb9ea072e58f46f51
sophiabeibei <duguyiren681@163.com>
2017-10-29 10:31:35 +0800

    node的express框架

---
+++ 201708_TeacherZhang/FirstWeek/20171029/routes/user.js
@@ -0,0 +1,15 @@
+/**
+ * GET /user/signup
+ * POST /user/signup
+ * GET /user/signin
+ * POST /user/signin
+ **/
+let express = require("express");
+let router = express.Router();
+router.get("/signup",function (req, res) {
+    //->此路径是相对于模板根目录的子路径,后缀是可以省略;
+    res.render("signup",{title: "用户注册"})
+});
+module.exports = router;
+
+
---
+++ 201708_TeacherZhang/FirstWeek/20171029/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const index = require("./routes/index");
+const user = require("./routes/user");
+const path = require("path");
+//=>设置模板引擎: 决定了模板后缀;
+app.set("view engine","html");
+//=>设置模板的存放根目录
+app.set("views",path.resolve("views"));
+//=>设置模板的渲染方法
+app.engine("html",require("ejs").__express);
+//=>当请求路径以/开头的时候,走index中间件
+app.use("/",index);
+//=>当请求路径以/user开头的时间,走user中间件;
+app.use("/user",user);
+app.listen(8080);
+
