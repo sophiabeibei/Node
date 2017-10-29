93ce2b47edafa20402d585151b852558f0f3c9c1
sophiabeibei <duguyiren681@163.com>
2017-10-29 12:23:59 +0800

    cookie统计客户端访问服务器的次数

---
+++ 201708_TeacherZhang/FirstWeek/20171029/cookie/2.express-cookie.js
@@ -0,0 +1,41 @@
+let express = require("express");
+//=>这个中间件能获取请求头中的cookie字段,并且把它转成一个字段,然后赋给req.cookies;
+//require("cookie-parser");
+let cookieParser = require("cookie-parser");
+let app = express();
+//=>使用此中间件之后,req请求对象多了一个cookies属性;
+app.use(cookieParser());
+app.get("/write",function (req, res) {
+    res.cookie("name","zfpx");
+    res.send("write ok");
+});
+
+app.get("/read",function (req, res) {
+    //->name=zfpx querystring.parse(str);
+    res.json(req.cookies);
+});
+//->统计每个客户端访问服务器的次数;
+app.get("/visit",function (req, res) {
+    //->1.先试图获取客户端的cookie里的访问次数;
+    let visit = req.cookies.visit || 0;
+    //->2.把访问次数+1;
+    visit++;
+    //->3.把最新的访问次数写回客户端;
+    res.cookie("visit");
+    //->写个响应体
+    res.send("欢迎你的第{visit}次访问");
+});
+app.listen(8080);
+
+
+
+
+
+
+
+
+
+
+
+
+
